d53393e2c27e48cdfe32dc3a07c63eafe4e9e5f9
Ben Zellers <zellersdenver@gmail.com>
2023-06-20 16:04:53 -0400

    fun with javascript

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ console.log(
 );
 
 // Exercise 1
+const firstname = "Mickey";
+let lastname = "Mouse";
+var age = 100
 
-// Exercise 2
+lastname = "Jackson"
+age = 78
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was gifted to the world in ${createdYear}`);
+
+let product = "Jacket";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`Wow, this ${product} is seriously only ${price}.
+ But wouldn't you know it the stock is ${stock}.`);
+
+ let title = "Name of the Wind";
+ let author = "Patrick Rothfuss";
+ let pageCount = 722;
+ let bookMark = 456;
+ let hasRead = true;
+
+ console.log(`It is ${hasRead} that ${firstname} has read ${title} and he told
+ me the number of pages were ${pageCount}. So I told him that ${language} came out
+ in the year ${createdYear}.`);
